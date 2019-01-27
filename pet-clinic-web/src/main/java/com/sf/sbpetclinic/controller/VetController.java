da0d5f016f123f2f637c800de52dd3682dd80ccc
rajesh-kum-kalyankar <rajesh.kum.kalyankar@microfocus.com>
2019-01-27 23:46:46 +0530

    Adding Vet and Owner controller, closes #5

---
+++ pet-clinic-web/src/main/java/com/sf/sbpetclinic/controller/VetController.java
@@ -0,0 +1,16 @@
+package com.sf.sbpetclinic.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("vet")
+public class VetController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String index(){
+
+        return "vet/index";
+    }
+}
