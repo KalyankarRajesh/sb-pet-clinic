6e25a588ae21f9155e90c4ac9bc893bf64fe1418
rajesh-kum-kalyankar <rajesh.kum.kalyankar@microfocus.com>
2019-01-27 18:23:07 +0530

    Adding Service Interfaces, closes #3

---
+++ pet-clinic-data/src/main/java/com/sf/sbpetclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package com.sf.sbpetclinic.services;
+
+import com.sf.sbpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/sf/sbpetclinic/services/VetService.java
@@ -0,0 +1,12 @@
+package com.sf.sbpetclinic.services;
+
+import com.sf.sbpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
